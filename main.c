77580aecfb2fdda08d8ffc98f26d2b6785af66b6
gemoore <gemoore@uw.edu>
2022-10-31 02:10:15 -0700

    Implementing rx_demod interrupt

--- main.c
+++ main.c
@@ -55,14 +55,19 @@
 
 #include <stdbool.h>
 #include <math.h>
+#include <string.h>
 
 /***** Definitions *****/
 #define MAX32660_EVSYS_LED								0
+
 #define GPIO_PORT_OUT               			PORT_0
 #define GPIO_PIN_OUT                			PIN_12
 
 #define RX_DEMODULATE_PORT_IN       			PORT_0
 #define RX_DEMODULATE_PIN_IN        			PIN_5
+
+#define GPIO_PORT_SQUARE_WAVE							PORT_0
+#define GPIO_PIN_SQUARE_WAVE							PIN_11
 
 #define CONT_TIMER 												MXC_TMR1   /* Timer used by ContinuousTimer */
 
@@ -75,6 +80,7 @@
 /************************* Variable Definitions *************************/
 
 gpio_cfg_t gpio_out;															// P0_12
+//gpio_cfg_t gpio_square_wave;															// P0_11
 gpio_cfg_t gpio_rx_demodulate;										// P0_5
 gpio_cfg_t gpio_interrupt_status_rx_demodulate;
 		 
@@ -84,12 +90,13 @@ gpio_cfg_t gpio_interrupt_status_rx_demodulate;
 bool is_mcu_powered_down = FALSE;
 float divisor = 1000000; //995851;
 bool TIMER_FINISHED = FALSE;
+bool is_rx_demod_counter_running = FALSE;
 /**
  * Enumeration type for system state select.
  */
 	typedef enum {
 		POWERED_DOWN_MCU_AFE4404,
-		POWERED_DOWN_AFE4404,
+		RX_DEMODULATE,
 		ACTIVE_ALL
 	} system_state_t;
 	
@@ -105,39 +112,83 @@ bool TIMER_FINISHED = FALSE;
 //			GPIO_OutSet(&gpio_out);
 ////		LED_Off(0);
 //}
-
-void gpio_isr_rx_demod(void *cbdata) // I2C interrupt will be triggered by ADC_RDY going High for 200ns
+double running_avg = 0;
+#define MAX_RX_DEMOD_SEQ_BITS				16
+#define RX_DEMOD_PREAMBLE_BITS			8
+uint32_t elapsed_time[MAX_RX_DEMOD_SEQ_BITS];
+bool rx_demod_sequence_level[MAX_RX_DEMOD_SEQ_BITS];
+uint16_t rx_demod_isr_cnt = 0;
+bool rx_demod_is_preamble_good = FALSE;
+uint32_t rx_demod_bit_period_us = 1e6;
+void gpio_isr_rx_demod(void *cbdata)
 {
-	static uint16_t isr_cnt = 0;
-	isr_cnt++;
-	printf("\nGPIO ISR: %d\n", isr_cnt);
-	LED_Toggle(MAX32660_EVSYS_LED);
-	GPIO_OutToggle(&gpio_out);
+	static uint32_t running_sum_elapsed_time = 0;
+	if(rx_demod_isr_cnt == 0){
+		TMR_SW_Start(MXC_TMR2, 0);
+		running_sum_elapsed_time = 0;
+		for(int8_t idx = MAX_RX_DEMOD_SEQ_BITS-1; idx >= 0; idx--){
+			rx_demod_sequence_level[idx] = 0;
+			elapsed_time[idx] = 0;
+		}
+//		printf("\n\n");
+		//Avoiding starting on a one to zero transition
+		if(!((bool)GPIO_InGet(&gpio_rx_demodulate)))
+			rx_demod_isr_cnt++;
+		else
+			rx_demod_isr_cnt = 0;
+		return;
+	}
+	else if (rx_demod_isr_cnt > MAX_RX_DEMOD_SEQ_BITS){
+		TMR_SW_Stop(MXC_TMR2);
+		return;
+	}
 	
-//	int64_t rx_demod_period = SysTick->VAL - systick_prev;
-//	uint8_t bits = 0;
-//	static uint8_t bit_idx = 0;
-//	if(rx_demod_period > RX_DEMOD_PERIOD*(1-RX_DEMOD_ALLOWABLE_ERROR) && rx_demod_period < RX_DEMOD_PERIOD*(1+RX_DEMOD_ALLOWABLE_ERROR))
-//		rx_demod_toggle_cnt++;
-//	
-//	if (rx_demod_toggle_cnt >= RX_DEMOD_TOGGLE_CNT_MIN && FALSE == rx_demod_data_rdy){
-//		rx_demod_record_data = TRUE;
+	// Checking the preamble
+	if(RX_DEMOD_PREAMBLE_BITS == rx_demod_isr_cnt){
+		rx_demod_is_preamble_good = TRUE;
+
+		for(int8_t idx = RX_DEMOD_PREAMBLE_BITS-1; idx >= 0; idx--){
+			if(idx > 0){
+				if(rx_demod_sequence_level[idx] == rx_demod_sequence_level[idx-1]){
+					rx_demod_is_preamble_good = FALSE;
+					rx_demod_isr_cnt = 0;
+					TMR_SW_Stop(MXC_TMR2);
+					printf("Bad Preamble at %d|%d and %d|%d\n",idx,(int8_t) rx_demod_sequence_level[idx], idx-1,(int8_t) rx_demod_sequence_level[idx-1]);
+					for(int8_t idx = MAX_RX_DEMOD_SEQ_BITS-1; idx >= 0; idx--){
+						printf("%d,", (uint8_t)rx_demod_sequence_level[idx]);
+					}
+					printf("\n");
+					return;
+				}
+			}
+		}
+		rx_demod_bit_period_us = round(running_sum_elapsed_time/(float)(RX_DEMOD_PREAMBLE_BITS-1));
+//		printf("\nrx_demod_bit_period_us = %d\n\n", rx_demod_bit_period_us);
+	}
+
+//	LED_Toggle(MAX32660_EVSYS_LED);
+//	GPIO_OutToggle(&gpio_out);
+	memmove(&rx_demod_sequence_level[1], &rx_demod_sequence_level[0], (MAX_RX_DEMOD_SEQ_BITS-1)*sizeof(bool));
+	rx_demod_sequence_level[0] = (bool)GPIO_InGet(&gpio_rx_demodulate);
+	
+	memmove(&elapsed_time[1], &elapsed_time[0], (MAX_RX_DEMOD_SEQ_BITS-1)*sizeof(uint32_t));
+	elapsed_time[0] = TMR_TO_Elapsed(MXC_TMR2)-running_sum_elapsed_time; //TMR_SW_Stop(MXC_TMR2); //??? TMR_TO_Elapsed(MXC_TMR2)
+	running_sum_elapsed_time = running_sum_elapsed_time + elapsed_time[0];
+	
+//	for(int8_t idx = MAX_RX_DEMOD_SEQ_BITS-1; idx >= 0; idx--){
+//		printf("%d,", elapsed_time[idx]);
 //	}
-//	else if(TRUE == rx_demod_record_data && rx_demod_toggle_cnt < sizeof(uint32_t)){
-//		bits = round(rx_demod_period/(float)RX_DEMOD_PERIOD);
-//		while(bits > 0){
-//			bits--;
-//			bitWrite(rx_demod_data, bit_idx, GPIO_InGet(&gpio_rx_demodulate));
-//			bit_idx++;
-//		}
+//	printf("\n");
+//	for(int8_t idx = MAX_RX_DEMOD_SEQ_BITS-1; idx >= 0; idx--){
+//		printf("%d,", (uint8_t)rx_demod_sequence_level[idx]);
 //	}
-//	else if(TRUE == rx_demod_record_data && bit_idx >= sizeof(uint32_t)){
-//		rx_demod_data_rdy  = TRUE;
-//		rx_demod_toggle_cnt = 0;
-//		bit_idx = 0;
-//	}
-//	
-//	systick_prev = SysTick->VAL;
+//	printf("\n");
+		
+	rx_demod_isr_cnt++;
+
+		
+	//	printf("\nGPIO ISR Count: %d\n", rx_demod_isr_cnt);
+	// !!! ToDo: Correct for TMR2 overrun
 }
 
 void ContinuousTimer_Handler(void)
@@ -175,17 +226,13 @@ void power_up_mcu(void){
 
 void ContinuousTimer(uint32_t period_ticks)
 {
-    tmr_cfg_t tmr; 
-
+    tmr_cfg_t tmr;
     TMR_Disable(CONT_TIMER);
-    
     TMR_Init(CONT_TIMER, TMR_PRES_1, 0);
-    
     tmr.mode = TMR_MODE_CONTINUOUS;
     tmr.cmp_cnt = period_ticks;
     tmr.pol = 0;
     TMR_Config(CONT_TIMER, &tmr);
-
     TMR_Enable(CONT_TIMER);
 }
 
@@ -195,7 +242,7 @@ int main(void)
 		/* Setup output pin on P0_12. */
 		gpio_out.port = GPIO_PORT_OUT;
     gpio_out.mask = GPIO_PIN_OUT;
-    gpio_out.pad = GPIO_PAD_PULL_UP;
+    gpio_out.pad = GPIO_PAD_NONE;
     gpio_out.func = GPIO_FUNC_OUT;
     GPIO_Config(&gpio_out);
 		GPIO_OutClr(&gpio_out);
@@ -208,7 +255,7 @@ int main(void)
 		GPIO_Config(&gpio_rx_demodulate);
 
 		GPIO_RegisterCallback(&gpio_rx_demodulate, gpio_isr_rx_demod, &gpio_interrupt_status_rx_demodulate);
-		GPIO_IntConfig(&gpio_interrupt_status_rx_demodulate, GPIO_INT_EDGE, GPIO_INT_BOTH);
+		GPIO_IntConfig(&gpio_rx_demodulate, GPIO_INT_EDGE, GPIO_INT_BOTH);
 		GPIO_IntEnable(&gpio_rx_demodulate);
 		NVIC_EnableIRQ((IRQn_Type)MXC_GPIO_GET_IRQ(RX_DEMODULATE_PORT_IN));
 	
@@ -217,7 +264,7 @@ int main(void)
 	
 //		SYS_Clock_Select(SYS_CLOCK_NANORING, MXC_TMR0);
 		
-		uint32_t us_delay = 100; //round(340000/2);
+		uint32_t us_delay = 5000000; //round(340000/2);
 		uint32_t us_delay_calibrated = round(LP_HFIO_CLK_POWER_DOWN_SLOPE*((float)us_delay) + LP_HFIO_CLK_POWER_DOWN_INTERCEPT);
 		float measure_frequency = .3;
 		uint32_t interstitial_period_us = round(1/measure_frequency*1000000);
@@ -226,7 +273,8 @@ int main(void)
 //    error = SYS_SysTick_Config(sysTicks, USE_SYSTEM_CLK, MXC_TMR0);
 		
     printf("SysTick (SystemCoreClock) Clock = %d Hz\n", SYS_SysTick_GetFreq());
-		printf("PeripheralClock = %d Hz\n", PeripheralClock);
+		printf("Peripheral Clock = %d Hz\n", PeripheralClock);
+		printf("Timer Clock = %d Hz\n", SYS_TMR_GetFreq(MXC_TMR0));
 		printf("Delay = %dus\n", us_delay);
 		printf("Delay Calibrated = %dus\n", us_delay_calibrated);
 		uint32_t ticks = (uint32_t)(((double)us_delay_calibrated * (double)SystemCoreClock) / divisor);
@@ -235,14 +283,15 @@ int main(void)
 		printf("Interstitial Period Calibrated = %dus\n", interstitial_period_us_calibrated);
 		ticks = (uint32_t)(((double)interstitial_period_us_calibrated * (double)SystemCoreClock) / divisor);
 		printf("Interstitial Period Ticks TMR1 = %d\n", ticks);
+		
     
     if (error != E_NO_ERROR) {
         printf("ERROR: Ticks is not valid");
         LED_On(2);
     }
-    
+    LED_Off(MAX32660_EVSYS_LED);
     is_mcu_powered_down = FALSE;
-		system_state_t sys_state = POWERED_DOWN_MCU_AFE4404;
+		system_state_t sys_state = RX_DEMODULATE;
     NVIC_SetVector(TMR1_IRQn, ContinuousTimer_Handler);
     NVIC_EnableIRQ(TMR1_IRQn);
     while (1) {
@@ -259,8 +308,39 @@ int main(void)
 					mxc_delay(us_delay_calibrated);
 					sys_state = ACTIVE_ALL;
 					break;
-				case POWERED_DOWN_AFE4404:
+				case RX_DEMODULATE:
+//					GPIO_OutToggle(&gpio_square_wave);
 					mxc_delay(us_delay_calibrated);
+//					for(uint8_t idx = 0; idx < MAX_RX_DEMOD_SEQ_BITS; idx++){
+//						running_avg = running_avg+elapsed_time[idx];
+//						printf("%d,", elapsed_time[idx]);
+//					}
+//					printf("\n");
+//					for(uint8_t idx = 0; idx < MAX_RX_DEMOD_SEQ_BITS; idx++){
+//						printf("%d,", rx_demod_sequence_level[idx]);
+//					}
+//					printf("\n");
+//					running_avg = (running_avg+elapsed_time)/(double)2.0;
+					running_avg = running_avg/MAX_RX_DEMOD_SEQ_BITS;
+//					printf("\rRunning Average: %g\n", running_avg);
+					if(rx_demod_isr_cnt >= MAX_RX_DEMOD_SEQ_BITS || TMR_TO_Elapsed(MXC_TMR2) > rx_demod_bit_period_us*MAX_RX_DEMOD_SEQ_BITS*1.1){
+						
+						//manchester_decode(&elapsed_time[0], &rx_demod_sequence_level[0], rx_demod_isr_cnt);
+						//afe4404_reg_write (uint8_t reg_address, uint32_t data) //Note: largest possible data entry = LHS 20 DYNAMIC1_FIELD (e.g. 21st bit)
+						printf("\nrx_demod_bit_period_us = %d\n\n", rx_demod_bit_period_us);
+						printf("RX Demodulate Sequence: ");
+						for(int8_t idx = MAX_RX_DEMOD_SEQ_BITS-1; idx >= 0; idx--){
+							running_avg = running_avg+elapsed_time[idx];
+							printf("%d,", elapsed_time[idx]);
+						}
+						printf("\n");
+						for(int8_t idx = MAX_RX_DEMOD_SEQ_BITS-1; idx >= 0; idx--){
+							printf("%d,", (uint8_t)rx_demod_sequence_level[idx]);
+						}
+						printf("\n");
+						rx_demod_isr_cnt = 0;
+						TMR_SW_Stop(MXC_TMR2);
+					}
 					break;
 				case ACTIVE_ALL:
 //					ticks = (uint32_t)(((double)us*4 * (double)SystemCoreClock) / divisor);
